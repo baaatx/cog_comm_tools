0498eb403d2f4930dbb36e684d9e93615f6ba384
BrianArmstrong <brain@mail.utexas.edu>
2010-10-12 15:01:40 -0500

    added note

--- +cog_comm_tools/drawImageStim.m
+++ +cog_comm_tools/drawImageStim.m
@@ -1,5 +1,8 @@
 % Draws image data in 'imageStim' to window  BUT DOES NOT FLIP SCREEN.
 %
+% Note: The imageStim is drawn to its own destRect (its position is
+% encapsulated)
+%
 % Remember: TOP LEFT CORNER OF SCREEN is x=0, y=0.
 %
 % Author: Brian Armstrong
