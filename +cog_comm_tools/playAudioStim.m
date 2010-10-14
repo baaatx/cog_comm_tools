359b45bd9647e2b5f9f35c3be4b4eb9353399287
BrianArmstrong <brain@mail.utexas.edu>
2010-10-14 12:27:28 -0500

    new function for playing AudioStims

---
+++ +cog_comm_tools/playAudioStim.m
@@ -0,0 +1,12 @@
+% Function that plays the WAV data inside of an audioStim object and waits
+% for an amount of time equal to the length of the wav data before
+% continuing.
+%
+% Author: Brian Armstrong
+%
+function playAudioStim(audioStim)
+    % play the wav data
+    sound (audioStim.audioSamples, audioStim.audioFileSize);
+    
+    % wait
+    WaitSecs(audioStim.audioLength);
