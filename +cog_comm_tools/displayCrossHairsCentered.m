c718b3d63bb76760637ed819bcf2ce2017eb7862
BrianArmstrong <brain@mail.utexas.edu>
2010-10-15 17:36:49 -0500

    wrapper to show cross hairs

---
+++ +cog_comm_tools/displayCrossHairsCentered.m
@@ -0,0 +1,6 @@
+% Wrapper that Displays cross hairs centered.
+%
+% Author: Brian Armstrong
+%
+function displayCrossHairsCentered(window)
+    cog_comm_tools.displayImageAtDestRect(window, 'stimuli/images/cross_hairs.jpg', []);
