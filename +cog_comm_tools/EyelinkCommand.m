06526dc3740d071842a22a4c14c67651b7fdb0cd
BrianArmstrong <brain@mail.utexas.edu>
2010-10-19 16:51:09 -0500

    wrapper for eyelink

---
+++ +cog_comm_tools/EyelinkCommand.m
@@ -0,0 +1,12 @@
+% Wrapper to send Eyelink equipment a command string.
+%
+% Author: Brian Armstrong
+%
+function status = EyelinkCommand(command)
+    % send command
+    status = Eyelink('Command', command);
+    
+    % handle error
+    if (status < 0)
+        error (['There was an error sending\n\n' command '\n\nto Eyelink']);
+    end    
