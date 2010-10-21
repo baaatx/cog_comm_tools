1a850c8d440b19abeb63dcb181eb75a32ec3c703
BrianArmstrong <brain@mail.utexas.edu>
2010-10-21 16:50:02 -0500

    test

--- +cog_comm_tools/initializeEyeLink.m
+++ +cog_comm_tools/initializeEyeLink.m
@@ -2,9 +2,19 @@
 %
 % Author: Brian Armstrong
 %
-function initializeEyeLink()
-    status = Eyelink('Initialize');
+function initializeEyelink(resolution)
+    % call initialize
+    %status = Eyelink('Initialize');
+    % EyelinkInit is an 'Eyelink oneliner'
+    [result, dummy ] = EyelinkInit(0, 1);
     
-    if (status == -1)
+    % handle error
+    if (result ~= 1)
         error ('There was a failure initializing EyeLink');
     end
+    
+    % set the tracker ro use our screen's resolution
+    status = cog_comm_tools.EyelinkSetResolution(resolution.width,resolution.height);
+    
+    % make sure that we get gaze data from the Eyelink (from ETB)
+    Eyelink('Command', 'link_sample_data = LEFT,RIGHT,GAZE,AREA');   
