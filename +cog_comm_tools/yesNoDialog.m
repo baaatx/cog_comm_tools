a66221b3339495dbe454474712686c71e8aec7c3
BrianArmstrong <brain@mail.utexas.edu>
2010-10-27 13:03:24 -0500

    changed from change

--- +cog_comm_tools/yesNoDialog.m
+++ +cog_comm_tools/yesNoDialog.m
@@ -3,8 +3,8 @@
 % Author: Brian Armstrong
 %
 function answer = yesNoDialog(window, questionMessage)
-    yes = cog_comm_tools.Choice('y', KbName('y'), 'Yes');
-    no = cog_comm_tools.Choice('n', KbName('n'), 'No');
+    yes = cog_comm_tools.Choice('y', 'Yes');
+    no = cog_comm_tools.Choice('n', 'No');
     choiceList = [yes no ];    
     answer = cog_comm_tools.multipleChoiceDialog(window, questionMessage , choiceList);
     if (strcmp(answer, 'Yes'))
