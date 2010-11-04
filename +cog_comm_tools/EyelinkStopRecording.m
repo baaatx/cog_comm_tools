087a8c49164d40e02a74184b8ab3dd5e2c5eb854
BrianArmstrong <brain@mail.utexas.edu>
2010-11-04 15:44:15 -0500

    new eyelink wrappers

---
+++ +cog_comm_tools/EyelinkStopRecording.m
@@ -0,0 +1,7 @@
+% Wrapper for sending the STOP RECORDING command to the Eyelink Host
+% computer...
+%
+% Author: Brian Armstrong
+%
+function EyelinkStopRecording()
+     Eyelink('StopRecording');
---
+++ +cog_comm_tools/EyelinkTrialOk.m
@@ -0,0 +1,6 @@
+% Wrapper for sending the TRIAL OK message to the Eyelink Host Computer.
+%
+% Author: Brian Armstrong
+%
+function EyelinkTrialOk()
+     Eyelink('Message', 'TRIAL OK ');
