9717fed5c5d3c0d0211e232e38846f6d28269f4f
BrianArmstrong <brain@mail.utexas.edu>
2010-11-17 14:20:05 -0600

    new multiple choice dialog with no output

---
+++ +cog_comm_tools/multipleChoiceDialogNoDisplay.m
@@ -0,0 +1,34 @@
+% A textual multiple choice dialog function that reports the chosen answer
+% (does not display anything)
+%
+% choiceList - the list of Choice objects
+%
+%
+% Author: Brian Armstrong
+%
+function [answer, responseTime ]= multipleChoiceDialogNoDisplay(choiceList)
+    
+    answer = 'notSelected';
+    answerInvalid = true;
+    
+    % supress text from matlab window
+    ListenChar(2);
+    
+    % wait for a valid choice to be selected from the keyboard
+    while (answerInvalid)
+        KbWait();
+        for i=1:length(choiceList)
+            if (cog_comm_tools.checkForKeyPress(choiceList(i).keyCode))
+                answerInvalid = false;
+                answer = choiceList(i).value;
+            end
+        end
+        % allow halt with ESC key
+        cog_comm_tools.checkForEscapeKeyToHalt();
+    end
+    
+    % calculate response time
+    responseTime = GetSecs() - startTime;
+    
+    % un-supress text from matlab window
+    ListenChar(1);
