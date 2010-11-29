e8bc7aff7ccfcc94e92e11848aaae175e7710670
BrianArmstrong <brain@mail.utexas.edu>
2010-11-29 13:47:52 -0600

    use fontSize to better approx centering of title

--- +cog_comm_tools/drawImageStimWithTitle.m
+++ +cog_comm_tools/drawImageStimWithTitle.m
@@ -8,5 +8,13 @@
 % Author: Brian Armstrong
 %
 function drawImageStimWithTitle(window, imageStim)
+
+    % get font size for window
+    fontSize = Screen('TextSize', window);
+
+    % draw the image stim data to the screen...
     cog_comm_tools.drawImageDataAtDestRect(window, imageStim.imageData, imageStim.destRect);
-    DrawFormattedText(window, imageStim.title, (imageStim.xPos - imageStim.width/4), (imageStim.yPos + imageStim.height/2), 0);
+
+    % draw the title under the image (approx centered)
+    xOffset = ((fontSize/2) * round(length(imageStim.title) / 2));
+    DrawFormattedText(window, imageStim.title, (imageStim.xPos - xOffset), (imageStim.yPos + imageStim.height/2), 0);
