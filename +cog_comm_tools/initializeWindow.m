41b13808948775627d4f57f44d596d0a120aae71
BrianArmstrong <brain@mail.utexas.edu>
2010-11-29 13:56:17 -0600

    change call back to obtain resolution

--- +cog_comm_tools/initializeWindow.m
+++ +cog_comm_tools/initializeWindow.m
@@ -6,26 +6,32 @@
 %
 % fontStyle  = Font Style.
 %
-% desiredRes = resolution to try to set screen to
+% desiredRes = (optional) resolution to attempt to set screen to...
 %
 %
 % Author: Brian Armstrong
 %
-function [window, res] = initializeWindow(fontName, fontSize, fontStyle, desiredRes)
+function [window, resolution] = initializeWindow(fontName, fontSize, fontStyle, desiredRes)
+    
     % Choosing the display with the highest display number is a best guess about where you want the stimulus displayed.
     screens=Screen('Screens');
     screenNumber=max(screens);
 
+    % provide a default resolution
     if (nargin < 4)
-        desiredRes = [800 600];
+        desiredRes = [1024 768];
     end
     
-    res=NearestResolution(screenNumber,desiredRes);
+    % set window to resolution 'nearest' to desired resolution
+    NearestResolution(screenNumber,desiredRes);
     
-    % Open window with default settings:
+    % Open window with default settings
     window=Screen('OpenWindow', screenNumber);
 
-    % Select specific text font, style and size:
+    % call back and check the resolution
+    resolution=Screen('Resolution', window); 
+    
+    % set text font, style and size
     Screen('TextFont',window, fontName);
     Screen('TextSize',window, fontSize);
     Screen('TextStyle', window, fontStyle);   
