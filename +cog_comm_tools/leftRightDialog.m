e49a2badf7f0766222f87e52fc839c8645034439
BrianArmstrong <brain@mail.utexas.edu>
2010-11-30 16:45:52 -0600

    uses new wrapper for padding

--- +cog_comm_tools/leftRightDialog.m
+++ +cog_comm_tools/leftRightDialog.m
@@ -12,17 +12,16 @@ function [answer, responseTime] = leftRightDialog(window, questionMessage)
     % clear the window (to make transition cleaner)
     cog_comm_tools.clearWindow(window);
 
-    % get the resolution from window
-    resolution=Screen('Resolution', window); 
-
+    [xPad, yPad] = getScreenPadding(window);
+    
     % use 'z' and '/?' keys for yes and no
     yes = cog_comm_tools.Choice('/?', 'Yes');
     no = cog_comm_tools.Choice('z', 'No');
     
     % draw question and options to the window buffer and draw the window
-    cog_comm_tools.drawTextAtPosition(window, questionMessage, 400, 400);
-    cog_comm_tools.drawTextAtPosition(window, 'Z: No' , 200 , resolution.height - 400);
-    cog_comm_tools.drawTextAtPosition(window, '/?: Yes' , resolution.width - 400 , resolution.height - 400);
+    cog_comm_tools.drawTextAtPosition(window, questionMessage, xPad, yPad);
+    cog_comm_tools.drawTextAtPosition(window, 'Z: No' , 200 , resolution.height - yPad);
+    cog_comm_tools.drawTextAtPosition(window, '/?: Yes' , resolution.width - (2*xPad) , resolution.height - yPad);
     cog_comm_tools.drawWindow(window);
 
     % send choiceList to multiple choice dialog function
