aea9dae06cb8f09d2603e48f43e03f645f28512e
BrianArmstrong <brain@mail.utexas.edu>
2010-12-14 11:42:03 -0600

    new wrapper for displaying an ImageStim object at screen position (xPos,yPos)

---
+++ +cog_comm_tools/displayImageStimAtPosition.m
@@ -0,0 +1,25 @@
+% Allows display of an ImageStim object at an arbitrary position on the
+% screen.
+%
+% Display image data in 'imageStim' to window at position (xPos , yPos).
+% 
+% window - the window pointer
+%
+% imageStim - an imageStim object
+%
+%
+% Author: Brian Armstrong
+%
+function destRect = displayImageStimAtPosition(window, imageStim, xPos, yPos)
+    
+    % calculate coordinates for destRect
+    minX = (xPos - (imageStim.width / 2));
+    minY = (yPos - (imageStim.height / 2));
+    maxX = (xPos + (imageStim.width / 2));
+    maxY = (yPos + (imageStim.height / 2));
+    
+    % create destRect row
+    destRect = [minX minY maxX maxY];
+
+    % display Image data with custom destRect
+    cog_comm_tools.displayImageDataAtDestRect(window, imageStim.imageData, destRect);
