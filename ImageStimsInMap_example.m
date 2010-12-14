bc732aad105c854ac9b17215c19a3a1605881711
BrianArmstrong <brain@mail.utexas.edu>
2010-12-14 12:49:29 -0600

    fixed typo in comments

--- ImageStimsInMap_example.m
+++ ImageStimsInMap_example.m
@@ -17,7 +17,7 @@ fontStyle = 1;
 imageDisplayTime = 1.0;
 screenResolution = [800 600];
 
-% a try block 'tries' a block of code and if an expection occurs it will jump to the following catch block 
+% a try block 'tries' a block of code and if an exception occurs it will jump to the following catch block 
 try
     % SETUP EXPERIMENT
     % assert that what we need is installed and working
