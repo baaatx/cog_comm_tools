e19d2aa5ce1837a3c885de672e2abc011562283e
BrianArmstrong <brain@mail.utexas.edu>
2010-12-28 17:43:03 -0600

    wrapper for transposing a matrix (rows to columns)

---
+++ +cog_comm_tools/transpose.m
@@ -0,0 +1,9 @@
+% Wrapper to return the transpose matrix of input matA.
+%
+% matA - the matrix to transpose
+%
+% Author: Brian Armstrong
+
+function matATranspose = transpose(matA)
+
+matATranspose = matA';
