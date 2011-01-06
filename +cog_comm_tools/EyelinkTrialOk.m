85b338e047a979888c9c3589df409343e62a4ed7
BrianArmstrong <brain@mail.utexas.edu>
2011-01-06 17:48:02 -0600

    comment

--- +cog_comm_tools/EyelinkTrialOk.m
+++ +cog_comm_tools/EyelinkTrialOk.m
@@ -1,5 +1,6 @@
 % Wrapper for sending the TRIAL OK message to the Eyelink Host Computer.
-%
+% 
+% Note: Do this at the end of every trial, before you stop recording...
 %
 % Author: Brian Armstrong
 %
