7f3651abfc5997a46bea984afed38054a2e1cebb
BrianArmstrong <brain@mail.utexas.edu>
2011-01-06 17:46:28 -0600

    added options for more control

--- +cog_comm_tools/displayTextAtPosition.m
+++ +cog_comm_tools/displayTextAtPosition.m
@@ -8,9 +8,42 @@
 %
 % y - the y position to draw to
 %
+% backgroundColor - (optional) background color (single int or RGB)
+%
+% fontColor - (optional) font color (single int for grayscale or an RGB [red green blue] value) 
+%
+% wrapAt - (optional) the number of characters to wrap into a new line.
+%
+% vSpacing - (optional) the vertical spacing between lines
+%
 %
 % Author: Brian Armstrong
 %
-function displayTextAtPosition(window, text, x, y)
-    DrawFormattedText(window, text, x, y, 0);
-    Screen('Flip', window);
+function displayTextAtPosition(window, text, x, y, backgroundColor, fontColor, wrapAt, vSpacing)
+    
+    % if a background color is passed in, fill the background before
+    % drawing text...
+    if (nargin > 4)
+        cog_comm_tools.fillWindow(window, backgroundColor);
+    end
+
+    % font color is optional, defaults to the initialized color value...
+    if (nargin < 6)
+        fontColor = Screen('TextColor', window);
+    end
+    
+    % wrap at (break into newlines) is optional
+    if (nargin < 7)
+        wrapAt = 50;
+    end
+    
+    % vertical Spacking is optional 
+    if (nargin < 8)
+        vSpacing = 1.5;
+    end
+            
+    % draw the text to the screen...
+    DrawFormattedText(window, text, x, y, fontColor, wrapAt, [], [], vSpacing);
+    
+    % display the screen's buffer
+    cog_comm_tools.displayWindow(window);
