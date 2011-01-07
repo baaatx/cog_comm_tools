9b28f6f3287b1e65d2f51dee091d4f08c1877eb5
BrianArmstrong <brain@mail.utexas.edu>
2011-01-07 13:20:51 -0600

    changed to add options

--- +cog_comm_tools/drawTextAtPosition.m
+++ +cog_comm_tools/drawTextAtPosition.m
@@ -8,17 +8,33 @@
 %
 % y - the y position to display to
 %
-% fontColor - (optional) a color vector... overrides the default font color
+% backgroundColor - (optional) background color (single int or RGB)
+%
+% fontColor - (optional) font color (single int for grayscale or an RGB [red green blue] value) 
+%
+% wrapAt - (optional) the number of characters to wrap into a new line.
+%
+% vSpacing - (optional) the vertical spacing between lines
 %
 %
 % Author: Brian Armstrong
 %
-function drawTextAtPosition(window, text, x, y, fontColor)
+function drawTextAtPosition(window, text, x, y, fontColor, wrapAt, vSpacing)
 
-    % font color is optional, defaults to initializes textColor value...
+    % font color is optional, defaults to the initialized color value...
     if (nargin < 5)
         fontColor = Screen('TextColor', window);
     end
     
-    % use PTB function to draw the text...
-    DrawFormattedText(window, text, x, y, fontColor);
+    % wrap at (break into newlines) is optional
+    if (nargin < 6)
+        wrapAt = cog_comm_tools.getWrapAt(window);
+    end
+    
+    % vertical Spacking is optional 
+    if (nargin < 7)
+        vSpacing = 1.5;
+    end
+            
+    % draw the text to the screen...
+    DrawFormattedText(window, text, x, y, fontColor, wrapAt, [], [], vSpacing);
