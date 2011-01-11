ebc0f5226d7b9caad9a530f7388416c288268ad2
BrianArmstrong <brain@mail.utexas.edu>
2011-01-11 15:18:34 -0600

    updated comments

--- +cog_comm_tools/checkForKeyPress.m
+++ +cog_comm_tools/checkForKeyPress.m
@@ -1,16 +1,19 @@
-% Checks for a press of a key with 'keyCode' on the keyboard.
+% Checks if a key with 'keyCode' is currently being pressed on the
+% keyboard.  This function does not wait.
 %
 % keyCode - 'ESCAPE' , 'a', '1', etc.
+%
 %
 % Author: Brian Armstrong
 %
 function isPressed = checkForKeyPress(keyCode)
     key = KbName(keyCode);
 
-    [ keyIsDown, seconds, keyCode ] = KbCheck;
+    isPressed = false;
+    
+    % call KbCheck for keyboard status...
+    [ keyIsDown, seconds, keyCode ] = KbCheck();
     
     if (keyIsDown && keyCode(key))
             isPressed = true;
-    else
-            isPressed = false;
     end   
