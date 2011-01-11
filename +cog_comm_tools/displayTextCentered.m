cba3d714401829408bbaf3812bda75421f60391a
BrianArmstrong <brain@mail.utexas.edu>
2011-01-11 17:17:18 -0600

    now wraps drawTextAtPosition

--- +cog_comm_tools/displayTextCentered.m
+++ +cog_comm_tools/displayTextCentered.m
@@ -4,9 +4,16 @@
 %
 % text - the text string to display
 %
+% fontColor - the RGB color to use for the text...
 %
 % Author: Brian Armstrong
 %
-function displayTextCentered(window, text)
-    DrawFormattedText(window, text, 'center', 'center', 0);
-    Screen('Flip', window);
+function displayTextCentered(window, text, fontColor)
+
+    % font color is optional, defaults to the initialized color value...
+    if (nargin < 3)
+        fontColor = Screen('TextColor', window);
+    end
+
+    cog_comm_tools.drawTextCentered(window, text, fontColor);
+    cog_comm_tools.displayWindow(window);
