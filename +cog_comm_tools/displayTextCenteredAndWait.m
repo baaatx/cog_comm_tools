afcfdea0ae96d61bffc9f11132f339f609a8b2da
BrianArmstrong <brain@mail.utexas.edu>
2011-01-11 17:17:42 -0600

    now wraps drawTextAtPosition

--- +cog_comm_tools/displayTextCenteredAndWait.m
+++ +cog_comm_tools/displayTextCenteredAndWait.m
@@ -6,10 +6,17 @@
 %
 % displayTime - the amount of time to display for before continuing.
 %
+% fontColor - (optional) the RGB color to use for the font
 %
 % Author: Brian Armstrong
 %
-function displayTextCenteredAndWait(window, text, displayTime)
-    DrawFormattedText(window, text, 'center', 'center', 0);
-    Screen('Flip', window);
+function displayTextCenteredAndWait(window, text, displayTime, fontColor)
+    
+    % font color is optional, defaults to the initialized color value...
+    if (nargin < 4)
+        fontColor = Screen('TextColor', window);
+    end    
+
+    cog_comm_tools.displayTextCentered(window, text, fontColor);
+    
     WaitSecs(displayTime);
