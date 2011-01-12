dbe9d01f05afe66b86a7bec4275de9e1eee9b1c0
BrianArmstrong <brain@mail.utexas.edu>
2011-01-12 12:30:33 -0600

    uses new generic key wrapper

--- +cog_comm_tools/checkForSpaceKeyToPause.m
+++ +cog_comm_tools/checkForSpaceKeyToPause.m
@@ -1,17 +1,9 @@
 % Checks to see if a space bar key was pressed, so the experimenter can pause the experiment.
+% Once the experiment is paused, the experimenter can press the space bar
+% again to resume the experiment.
+%
 %
 % Author: Brian Armstrong
 %
 function checkForSpaceKeyToPause()
-    spaceKey = KbName('space');
-    if cog_comm_tools.checkForKeyPress('space')
-        WaitSecs(2);
-        while true
-            [ keyIsDown, seconds, keyCode ] = KbCheck;
-            if keyCode(spaceKey)
-                break;
-            else
-                WaitSecs(0.2);
-            end
-        end
-    end    
+    cog_comm_tools.checkForKeyToPause('space');
