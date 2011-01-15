45c85dd6d43b5d494c16a7aebce9abca21397efa
BrianArmstrong <brain@mail.utexas.edu>
2011-01-15 16:26:12 -0600

    removed wrapper (changed to displayWindow)

--- +cog_comm_tools/drawWindow.m
+++
@@ -1,9 +0,0 @@
-% Wrapper for PTB Screen Flip (draws the video buffer to the display)
-%
-% window - the window to flip (draw buffer to screen)
-%
-%
-% Author: Brian Armstrong
-%
-function drawWindow(window)
-    Screen('Flip', window);
