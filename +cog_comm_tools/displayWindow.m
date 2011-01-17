8783247a6f722d3a68c0ebde577a6f8b8bcb8232
BrianArmstrong <brain@mail.utexas.edu>
2011-01-17 16:13:50 -0600

    comments

--- +cog_comm_tools/displayWindow.m
+++ +cog_comm_tools/displayWindow.m
@@ -6,4 +6,10 @@
 % Author: Brian Armstrong
 %
 function displayWindow(window)
+    % check for window pointer...
+    if (nargin ~= 1)
+        error ('no window pointer argument...');
+    end
+    
+    % PTB screen flip...
     Screen('Flip', window);
