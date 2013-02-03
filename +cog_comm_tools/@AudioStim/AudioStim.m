2e09a7a3e0ca07d0cad4f4985eda1f9241fb22f8
Brian Armstrong <baa.atx@gmail.com>
2013-02-03 11:00:25 -0600

    updated audioStim object - has properties and its own driver

--- +cog_comm_tools/@AudioStim/AudioStim.m
+++ +cog_comm_tools/@AudioStim/AudioStim.m
@@ -1,5 +1,6 @@
 % This is a handle class that represents a single audio stimulus used in an
-% experiment.
+% experiment. The Audio stim contains its own audio driver reference so
+% it can play itself.
 %
 % Constructor Parameters:
 %
@@ -22,6 +23,8 @@ classdef AudioStim < handle
       frequency
       bitsPerSecond
       numChannels
+      portAudioHandle
+      propertiesMap
    end
    properties  (SetAccess = public, GetAccess = public)
       title
@@ -46,7 +49,6 @@ classdef AudioStim < handle
            obj.keyCode = keyCode;
            obj.wavFileName = wavFileName;
            
-           
            % get the wave data
            [obj.audioSamples, obj.frequency, obj.bitsPerSecond] = wavread(wavFileName);
            
@@ -58,6 +60,44 @@ classdef AudioStim < handle
            
            % store the audio length...
            obj.audioLength = (length(obj.audioSamples)) / obj.frequency;
+           
+           % create an internal map so other properties and objects can
+           % be associated with this object
+           obj.propertiesMap = containers.Map();
+       end
+       function setProperty(obj, key, value)
+        obj.propertiesMap(key) = value;
+       end
+       function value = getProperty(obj, key)
+        value = obj.propertiesMap(key);
+       end
+       
+       function open(obj)
+        try
+            % Try with the frequency returned by wavread:
+            obj.portAudioHandle = PsychPortAudio('Open', [], [], 0, obj.frequency, obj.numChannels);
+        catch
+            % Failed. so we retry with the frequency as suggested by device:
+            psychlasterror('reset');
+            obj.portAudioHandle = PsychPortAudio('Open', [], [], 0, [], obj.numChannels);
+        end
+
+        % Fill the audio playback buffer with the audio data 'wavedata':
+        PsychPortAudio('FillBuffer', obj.portAudioHandle, obj.waveData);
+       end
+       function startTime = play(obj)
+        % Start audio playback for one time through the sound data,
+        % start it immediately (0) and wait for the playback to start, return onset
+        % timestamp.
+        startTime = PsychPortAudio('Start', obj.portAudioHandle, 1, 0, 1);
+       end
+       function stop(obj)
+        % Stop playback:
+        PsychPortAudio('Stop', obj.portAudioHandle);
+       end
+       function close(obj)
+        % Close the audio device:
+        PsychPortAudio('Close', obj.portAudioHandle);
        end
    end
 end % classdef
